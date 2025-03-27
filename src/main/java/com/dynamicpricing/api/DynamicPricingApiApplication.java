982e966bf34d54ef545465cbefa5c8e077752ad3
Sheed <rasheed.saka85@yahoo.com>
2025-03-27 15:25:02 -0500

    Spring Initializr Project Commit

---
+++ src/main/java/com/dynamicpricing/api/DynamicPricingApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicpricing.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPricingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPricingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamicpricing/api/DynamicPricingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamicpricing.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPricingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

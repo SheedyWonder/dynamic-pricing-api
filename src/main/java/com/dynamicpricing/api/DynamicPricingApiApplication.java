a3392f77e43f39e328bf05051bfe425de9a6ae6c
Sheed <rasheed.saka85@yahoo.com>
2025-03-31 13:00:03 -0500

    Testing if Application can be ran locally *

--- src/main/java/com/dynamicpricing/api/DynamicPricingApiApplication.java
+++ src/main/java/com/dynamicpricing/api/DynamicPricingApiApplication.java
@@ -2,8 +2,10 @@ package com.dynamicpricing.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.dynamicpricing.api")
 public class DynamicPricingApiApplication {
 
 	public static void main(String[] args) {
